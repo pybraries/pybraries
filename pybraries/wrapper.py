e3d9f2f0aa0a3a1d25d0b129907cd77618f371c9
discdiver <jeffmshale@gmail.com>
2019-05-10 15:22:09 -0400

    adds fire for cli

--- pybraries/wrapper.py
+++ pybraries/wrapper.py
@@ -1,7 +1,6 @@
 import requests
 import fire
 
-
 def __call_api(api_key, manager, package):
     """
     Call the API for package manager info.
@@ -15,11 +14,17 @@ def __call_api(api_key, manager, package):
     if not isinstance(api_key, str):
         raise TypeError("Please provide your api key as a string.")
 
-    r = requests.get(f'https://libraries.io/api/{manager}/{package}?api_key={api_key}')
-    return r.json()
+    r = requests.get(
+        f'https://libraries.io/api/{manager}/{package}',
+        params=dict(api_key=api_key),
+        timeout=3,
+    )
+    response = r.json()
+
+    return response
 
 
-def package_info(manager, package):
+def package_info(api_key, manager, package):
     """
     Print result of API for package manager info.
     Args:
@@ -29,7 +34,10 @@ def package_info(manager, package):
         r.json (json): response from libraries.io
     """
     result = __call_api(api_key, manager, package)
-    print(result)
+    print(result['name'])
+    return 
 
 
-package_info("Pypi", "pandas")
+if __name__ == "__main__":
+    fire.Fire()
+    
