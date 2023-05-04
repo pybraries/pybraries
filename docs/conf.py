59f6978ac4f90b34e52b68158338dc8f7c7dd766
Jeff Hale <discdiver@users.noreply.github.com>
2023-05-04 09:55:04 -0400

    Update copyright year in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "Pybraries"
-copyright = "2020, Jeff Hale"
+copyright = "2023, Jeff Hale"
 author = "Jeff Hale"
 
 
