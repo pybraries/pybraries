d6499717bc98a92c3d304aa3327559de11cc9bd9
Jeff Hale <discdiver@users.noreply.github.com>
2023-05-04 09:57:13 -0400

    Update python versions in setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: BSD License",
     ],
     include_package_data=True,
